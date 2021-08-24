03dc35e784fbf7cd082085b7ab4699c2c1a76049
EHER.MZA <ehermza@yahoo.com>
2021-08-24 00:37:17 -0300

    OK: update container form by post method, unlink client to container.

--- client/src/app/components/form-container/form-container.component.ts
+++ client/src/app/components/form-container/form-container.component.ts
@@ -1,18 +1,64 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
+import { Contenedor } from 'src/app/models/Contenedor';
+
 
 @Component({
   selector: 'app-form-container',
   templateUrl: './form-container.component.html',
   styleUrls: ['./form-container.component.css']
 })
+
 export class FormContainerComponent implements OnInit {
 
   @Input() CONTAINER: any= {};
 
-    constructor() { }
+    idCtner:string= '';
+    model:Contenedor = new Contenedor();
+    submitted = false;
+
+    constructor(private http:HttpClient) { }
   
     ngOnInit(): void {
     }
+
+    ngOnChanges(changes: SimpleChanges)  
+    {
+      this.idCtner = this.CONTAINER._id;
+      console.log(`(formContainer) CONTAINER.get() `, this.CONTAINER);
+      // this.model.setId(this.CONTAINER._id);
+      this.model.setAtributtes(
+        this.CONTAINER._id,
+        this.CONTAINER.id_container,
+        this.CONTAINER.price_tocharge,
+        this.CONTAINER.rented_by
+      )
+    }
+
+    unlinkClient() 
+    {
+      this.submitted = true;
+
+      this.model.Unlinked();
+      console.log(`unlink id container: ${this.model._id} `);
+      // touch backend! router.get('/containers/unlink/:idcont', async function (req, res) 
+      this.http.put<any>(`/api/containers/${this.idCtner}`, this.model)
+        .subscribe(
+          data => { console.log('(unlinkClient) Subscribe: ', data); }
+        );
+    }
+
+    updatePrice()
+    {
+      if(this.submitted) return;
+      this.submitted = true;
+
+      this.http.put<any>(`/api/containers/${this.idCtner}`, this.model)
+        .subscribe(
+          data => { console.log('(updatePrice) Subscribe: ', data); }
+        );
   
+  
+    }
   }
   
---
+++ client/src/app/models/Contenedor.ts
@@ -0,0 +1,25 @@
+
+export class Contenedor 
+{
+    _id: string = '';
+    id_container: Number = -1;
+    price_tocharge: Number = 0;
+    rented_by: string = '';
+    rented_by_id?: string;
+    description?: string;
+    active?: boolean = false;
+
+    constructor() { }
+
+    public setAtributtes(_id: string, numberCtdor: Number, value: Number, client:string) {
+        this._id = _id;
+        this.id_container= numberCtdor;
+        this.price_tocharge= value;
+        this.rented_by = client;
+    }
+    public Unlinked() {
+        this.rented_by = '';
+        this.rented_by_id= '0';
+        this.active = false;
+    }
+}
