9552d0a189ed50ea9d821595b4ce5c0535df97b4
EHER.MZA <ehermza@yahoo.com>
2021-09-15 00:29:30 -0300

    Backup: Dump 'Containers' database by Server

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,12 +8,6 @@ import { MatTableModule } from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TableComponent } from './components/clients/table/table.component';
-import { NavigatorComponent } from './components/navigator/navigator.component';
-import { FormAddClientComponent } from './components/clients/form-add-client/form-add-client.component';
-// import { FormClientComponent } from './components/form-client/form-client.component';
-// import { FormContainerComponent } from './components/containers/form-container/form-container.component';
-
 import { ViewsModule } from './views/views.module';
 import { ComponentsModule } from './components/components.module';
 
